c51948c0e790803ddd5037b6cb4fc28ab7b2f503
ghostmatem <bebe558812@gmail.com>
2022-06-03 02:44:52 +0300

    Долгая и мучительная первая верстка

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
 import React from 'react';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+function App() {
+  return (
+      <body>
+        <nav><p>Q</p></nav>
+        <main>
+            <h1>Hi</h1>
+            <script src="test.js"></script>
+        </main>
+      </body>
+  );
+}
 
-// export default App;
+export default App;
